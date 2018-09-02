50ac78ca6da00207e591f494c07d37b845c9a79f
ihamzakbar <ihamzakbar@gmail.com>
2018-09-02 08:39:06 +0500

    adding the blog model for the first time

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=32)
+    datetime = models.DateTimeField(auto_now=True)
+    blog_body = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='images/')
+# Create a Blog model
+# Title
+# pub_date
+# body
+# image
+
+
+
+# add the blog app to settings
+# create migration
+# migrate
+# add to admin
+
